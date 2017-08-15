59dff7afd153172ebe2cf7647b6b540e15baefbd
lukasz.lukowicz <lukasz.lukowicz@geosolution.pl>
2017-08-15 15:16:36 +0200

    commit4

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -2,30 +2,36 @@ package application;
 
 import data.DataInitializer;
 import exception.NotFoundException;
+import model.Facebook;
 import service.FacebookService;
 import service.FacebookServiceImpl;
+
+import java.util.Map;
+import java.util.Set;
+
 //pl.application.facebook
 public class App 
 {
     public static void main( String[] args )
-    {  //BasicConfigurator.configure();
+    {
         //inicjacja  danych
         DataInitializer dataInitializer = new DataInitializer();
         dataInitializer.initializeData("src/resources/datapaths.txt");
 
         FacebookService facebookService = new FacebookServiceImpl();
 
-        facebookService.findMostCommonWords();
-        facebookService.findPostIdsByKeyword("loft");
-        facebookService.findPostIdsByKeyword("you");
-        facebookService.findPostIdsByKeyword("json");
-        facebookService.findPostIdsByKeyword("What");
-        facebookService.findAll();
+        Map<String, Long> wordMaps = facebookService.findMostCommonWords();
+        Set<String> postsIds1 = facebookService.findPostIdsByKeyword("loft");
+        Set<String> postsIds2 = facebookService.findPostIdsByKeyword("you");
+        Set<String> postsIds3 = facebookService.findPostIdsByKeyword("json");
+        Set<String> postsIds4 = facebookService.findPostIdsByKeyword("What");
+        Set<Facebook> allFacebookAccount = facebookService.findAll();
 
 
 
         try {
-           facebookService.findById("54");
+            Facebook profile1 = facebookService.findById("2");
+            Facebook profile2 = facebookService.findById("54");
         } catch (NotFoundException e) {
             e.getMessage();
         }
